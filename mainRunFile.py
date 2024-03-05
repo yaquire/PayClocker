28640f9991cd2142b3cb1b98bdb635a9f3ec52d2
yaq <yaqube147@gmail.com>
2024-03-05 23:38:58 +0800

    Have made the main file & actually started

---
+++ mainRunFile.py
@@ -0,0 +1,15 @@
+#This is meant to be used to have it so that every month, it creates a newfile (csv/other format) so that it easier to check, where it can also be made so that the info on wheter a week overlaps between two months, is pointed out & taken into account for the overlap.
+
+print('This is a test')
+
+def newFileCreation:
+    #Meant so that every month the file changes
+    print('File Made: ')
+
+def openingExsistingFile:
+    with open() as fileExisting:
+        curentData = fileExisting.readlines
+    print('File Opened')
+
+
+#This will check whether a new file has to be made or not!
