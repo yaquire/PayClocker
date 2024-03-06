b6dcbcd91ed5059ad2992f9349711f440ee17683
yaq <yaqube147@gmail.com>
2024-03-06 23:34:46 +0800

    I can say that i have started on the program :3

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # PayClocker
 This is used as a script to track my total Income
+
+{NOTE: CHATGPT has been used}
+- I used chatGPT as the tool to make this script, mostly so that i can also learn,, as chatGPT is not always right & can make mistakes as of writing this README
+- When i ran into an issue, i used google, to troubleshoot
+- I will annotate when a website/chatGPT gave me the line of code :)
--- mainRunFile.py
+++ mainRunFile.py
@@ -1,15 +1,26 @@
 #This is meant to be used to have it so that every month, it creates a newfile (csv/other format) so that it easier to check, where it can also be made so that the info on wheter a week overlaps between two months, is pointed out & taken into account for the overlap.
+import datetime
+
 
 print('This is a test')
 
-def newFileCreation:
+def newFileCreation():
     #Meant so that every month the file changes
     print('File Made: ')
 
-def openingExsistingFile:
+def openingExsistingFile():
     with open() as fileExisting:
         curentData = fileExisting.readlines
     print('File Opened')
 
 
 #This will check whether a new file has to be made or not!
+
+#this variable is the current date; got it from CHATGPT 
+#Its time is a class apparently,, will get back to that when i figure out the difference :)
+currentDate = datetime.date.today()
+#this works, has been checked
+
+#print('Current Date: ',(currentDate))
+
+
