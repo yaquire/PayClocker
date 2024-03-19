b857cae38c39ac15ef1a7da0366bca2235e968ff
yaq <yaqube147@gmail.com>
2024-03-19 23:15:13 +0800

    Clock In & Out use same function
    Made some changes so that the clocker fuction uses the input to tell if
    its clock In/Out so now i have one less chunck of code. I also has noot
    given any error so that is great

--- mainRunFile.py
+++ mainRunFile.py
@@ -26,60 +26,31 @@ def openingExsistingFile(fileName):
 
 
 # This will check whether a new file has to be made or not!
-
-
-def inputClock():
+def inputClocker(InOut):
     while True:
-        clockIn = input("Clock In Time (24-hr):")
+        clocker = input("Clock " + InOut + "  Time (24-hr):")
         try:
-            clockIn = int(clockIn)
+            clocker = int(clocker)
         except ValueError:
             print("!ERROR! Only Integer value")
 
         # after this; clockin is an int
-        clockIn = str(clockIn)
-        if len(clockIn) == 4:
+        clocker = str(clocker)
+        if len(clocker) == 4:
             if (
-                int(clockIn[0]) in possibleFirstFDigit
-                or int(clockIn[1]) in possibleNextDigits
-                or int(clockIn[2]) in possibleFMin
-                or int(clockIn[3]) in possibleNextDigits
+                int(clocker[0]) in possibleFirstFDigit
+                or int(clocker[1]) in possibleNextDigits
+                or int(clocker[2]) in possibleFMin
+                or int(clocker[3]) in possibleNextDigits
             ):
                 break
             else:
                 print("~Please Enter a Valid Time~")
         else:
             print("Please Enter a 4 digit No.")
-    print(clockIn)
+    print(clocker)
 
-    return clockIn
-
-
-def inputClockOut():
-    while True:
-        clockOut = input("Clock Out Time (24-hr):")
-        try:
-            clockOut = int(clockOut)
-        except ValueError:
-            print("!ERROR! Only Integer value")
-
-        # after this; clockin is an int
-        clockOut = str(clockOut)
-        if len(clockIn) == 4:
-            if (
-                int(clockOut[0]) in possibleFirstFDigit
-                or int(clockOut[1]) in possibleNextDigits
-                or int(clockOut[2]) in possibleFMin
-                or int(clockOut[3]) in possibleNextDigits
-            ):
-                break
-            else:
-                print("~Please Enter a Valid Time~")
-        else:
-            print("Please Enter a 4 digit No.")
-    print(clockOut)
-
-    return clockOut
+    return clocker
 
 
 def converter(insertedTime):
@@ -87,7 +58,6 @@ def converter(insertedTime):
     minute = insertedTime[2] + insertedTime[3]
 
     minuteFraction = int(minute) / 60
-    
 
     fixedTime = int(hour) + minuteFraction
     return fixedTime
@@ -95,19 +65,18 @@ def converter(insertedTime):
 
 def Calculator(clockIn, clockOut):
     payPerHour = 11
-    
-    
-    #this doesnt include break
-    breakTime=0
-    if clockIn<12:
-        breakTime +=1
-    if clockOut>19.5:
-        breakTime+=1
-    totalHours = clockOut-clockIn-breakTime
-    payForDay=payPerHour*totalHours
-    
+
+    # this doesnt include break
+    breakTime = 0
+    if clockIn < 12:
+        breakTime += 1
+    if clockOut > 19.5:
+        breakTime += 1
+    totalHours = clockOut - clockIn - breakTime
+    payForDay = payPerHour * totalHours
 
     return payForDay
+
 
 # this variable is the current date; got it from CHATGPT
 # Its time is a class apparently,, will get back to that when i figure out the difference :)
@@ -119,11 +88,11 @@ print(currentDateList)
 fileName = "Month:" + currentDateList[-2] + " Year:" + currentDateList[0] + ".csv"
 
 openingExsistingFile(fileName)
-clockIn = inputClock()
-clockOut = inputClockOut()
+clockIn = inputClocker("In")
+clockOut = inputClocker("Out")
 
 clockInFixed = converter(clockIn)
 clockOutFixed = converter(clockOut)
 
-payForDay = Calculator(clockInFixed,clockOutFixed)
-print('$', payForDay)
+payForDay = Calculator(clockInFixed, clockOutFixed)
+print("$", payForDay)
